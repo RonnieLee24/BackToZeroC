466b55766479999d9fa0a8d3c594143e748c1fad
Lilongqi <1255817186@qq.com>
2024-07-02 00:12:21 +0800

    Essential C++

---
+++ BackToZeroC/C++/Log.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+void Log(const char* message){
+    std::cout << message << std::endl;
+}
--- BackToZeroC/C++/test.cpp
+++ BackToZeroC/C++/test.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
-using namespace std;
-
+#include <iostream> //  预处理：在实际编译之前就被处理了
+#include "Log.h"
+using namespace std;    //  编译：编译器将所有 C++ 代码转化为实际机器代码
+//  连接 Link：将一些 编译后的生成的目标文件 obj 生成 exe 文件
 int main(){
-    cout<<"hello C+_+!!!"<<endl;
+    //  <<：重载运算符（可以看作函数）
+    Log("hello world");
     return 1;
 }
